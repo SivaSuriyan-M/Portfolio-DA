f0c7c677a06a8a788a5e76f7483465bd5d0d51f0
Siva Suriyan <suriya1571532@gmail.com>
2024-10-10 11:48:12 +0530

    Initial commit for portfolio DA

---
+++ src/assets/index.js
@@ -0,0 +1,133 @@
+import parthmittal from "./parth.svg";
+import profilePic from "./profile-pic.jpg";
+import quotes from "./quotes.svg";
+import robot from "./robot.png";
+import send from "./Send.svg";
+import shield from "./Shield.svg";
+import star from "./Star.svg";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import google from "./google.svg";
+import apple from "./apple.svg";
+import arrowUp from "./arrow-up.svg";
+import discount from "./Discount.svg";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import nitk from "./nitk.png";
+import cluboard from "./cluboard.png";
+import cash_flow from "./cash_flow.jpg";
+import freadom from "./freadom.jpg";
+import ecell from "./ecell.png";
+import bank from "./bank.jpg";
+import gdsc from "./gdsc.png";
+import iris from "./iris.png";
+import genesis from "./genesis.jpg";
+import todo from "./todo.jpg";
+import graphql from "./graphql.png";
+import portfolio from "./portfolio.jpg";
+import zulip from "./zulip.jpg";
+import publiclab from "./publiclab.png";
+import cdc from "./cdc.png";
+import chargeswap from "./chargeswap.png";
+import placeicon from "./place-icon.svg";
+import recruitment from "./recruitment.png";
+import huntly from "./huntly.png";
+import oracle from "./oracle.jpg";
+import comicify_ai from "./comicify-ai.png";
+import greentrust from "./green_trust.png";
+import averlon from "./averlon.jpg";
+import devfolio from "./devfolio.png";
+import pba from "./pba_logo.jpeg";
+import ethglobal from "./ethglobal.png";
+import polkadot from "./polkadot.jpeg";
+import lightspeed from "./lightspeed.png";
+import dennisivy from "./dennisivy.png";
+import manipal from "./manipal.png";
+import icon from "./icon.jpg";
+import ethforall from "./ethforall.png";
+import wlogo from "./wlogo.png";
+import Siva from "./Siva.png";
+import googlelogo from "./google-logo.svg";
+import oeclogo from "./oec-logo.png";
+import ibmlogo from "./ibm-logo.svg";
+import tcslogo from "./tcs-logo.svg";
+import codtech from "./codtech.jpg";
+import bj from "./bj.jpg";
+import marvel from "./marvel.jpg";
+import Layoffs1 from "./Layoffs1.jpg";
+import Layoffs from "./Layoffs.jpg";
+import deepfake from "./deepfake.png";
+import churn from "./churn.png";
+import airbnb from "./airbnb.jpg";
+import ev from "./EV.jpg";
+import segmen from "./segmen.png";
+
+export {
+  quotes,
+  robot,
+  send,
+  shield,
+  star,
+  menu,
+  close,
+  google,
+  apple,
+  arrowUp,
+  discount,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  nitk,
+  cluboard,
+  cash_flow,
+  freadom,
+  bank,
+  gdsc,
+  iris,
+  ecell,
+  genesis,
+  todo,
+  graphql,
+  portfolio,
+  publiclab,
+  zulip,
+  cdc,
+  parthmittal,
+  profilePic,
+  chargeswap,
+  placeicon,
+  recruitment,
+  huntly,
+  oracle,
+  comicify_ai,
+  greentrust,
+  averlon,
+  devfolio,
+  pba,
+  ethglobal,
+  polkadot,
+  lightspeed,
+  dennisivy,
+  manipal,
+  icon,
+  ethforall,
+  wlogo,
+  Siva,
+  oeclogo,
+  googlelogo,
+  ibmlogo,
+  tcslogo,
+  codtech,
+  bj,
+  marvel,
+  Layoffs1,
+  Layoffs,
+  deepfake,
+  churn,
+  airbnb,
+  segmen,
+  ev,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,746 @@
+import {
+  nitk,
+  cluboard,
+  cash_flow,
+  gdsc,
+  iris,
+  ecell,
+  genesis,
+  portfolio,
+  publiclab,
+  zulip,
+  cdc,
+  chargeswap,
+  placeicon,
+  recruitment,
+  huntly,
+  oracle,
+  comicify_ai,
+  greentrust,
+  averlon,
+  devfolio,
+  pba,
+  ethglobal,
+  polkadot,
+  lightspeed,
+  dennisivy,
+  manipal,
+  icon,
+  ethforall,
+  oeclogo,
+  googlelogo,
+  ibmlogo,
+  tcslogo,
+  codtech,
+  marvel,
+  deepfake,
+  bj,
+  Layoffs1,
+  Layoffs,
+  churn,
+  ev,
+  airbnb,
+  segmen,
+  
+
+} from "../assets";
+
+import {
+  AiFillGithub,
+  AiFillInstagram,
+  AiFillLinkedin,
+  AiFillMail,
+  AiOutlineTwitter,
+  AiFillHtml5,
+  AiOutlineGitlab,
+  AiTwotoneDatabase,
+  AiOutlineDatabase,
+  AiFillDatabase,
+  AiOutlineRadarChart,
+  AiFillCalculator,
+  AiOutlineAreaChart,
+  AiTwotonePieChart,
+  AiFillPieChart,
+  AiOutlineDotChart,
+  AiOutlineBarChart,
+  AiFillCode,
+  AiOutlineSearch,
+  AiOutlineTable,
+  AiFillRobot,
+  AiOutlineLoading,
+  AiOutlineCluster,
+} from "react-icons/ai";
+
+import {
+  SiDjango,
+  SiJavascript,
+  SiBootstrap,
+  SiReact,
+  SiTailwindcss,
+  SiGraphql,
+  SiPython,
+  SiCplusplus,
+  SiC,
+  SiRubyonrails,
+  SiJquery,
+  SiVisualstudiocode,
+  SiPostman,
+  SiGit,
+  SiMysql,
+  SiSolidity,
+  SiNetlify,
+  SiChartdotjs,
+  SiVite,
+  SiArduino,
+  SiWeb3Dotjs,
+  SiIpfs,
+  SiDotnet,
+  SiTwilio,
+  SiFlutter,
+  SiReplit,
+  SiFlask,
+  SiFigma,
+  SiGooglemaps,
+  SiOpenai,
+  SiGooglecloud,
+  SiNextdotjs,
+  SiMicrosoftazure,
+  SiR,
+  SiMicrosoftexcel,
+  SiGooglesheets,
+  SiGooglebigquery,
+  SiDgraph,
+  SiDatabricks,
+  SiJupyter,
+  SiBuffer,
+  SiWpexplorer,
+  SiTimescale
+} from "react-icons/si";
+
+import {
+  FaHardHat,
+  FaRust
+} from "react-icons/fa";
+
+import {
+  IoIosNotificationsOutline
+} from "react-icons/io";
+
+import {
+  FaGolang
+} from "react-icons/fa6";
+
+import { DiCss3, DiJava, DiMsqlServer, DiRuby } from "react-icons/di";
+import { SiSqlite } from "react-icons/si";
+import { SiTableau } from "react-icons/si";
+import { SiPowerbi } from "react-icons/si";
+import { SiGoogleanalytics } from "react-icons/si";
+import { SiSimpleanalytics } from "react-icons/si";
+import { SiVisualbasic } from "react-icons/si";
+import { SiSoundcharts } from "react-icons/si";
+import { SiStackedit } from "react-icons/si";
+import { SiEditorconfig } from "react-icons/si";
+import { SiCkeditor4 } from "react-icons/si";
+import { SiDatacamp } from "react-icons/si";
+import { SiOpensearch } from "react-icons/si";
+import { SiRobotframework } from "react-icons/si";
+
+export const resumeLink = "https://drive.google.com/file/d/1ZKX7_KEG0EDg4LA10L1d7rXf8nW9Vcgc/view?usp=drive_link";
+export const repoLink = "";
+
+export const callToAction = "https://www.linkedin.com/in/siva-suriyan";
+
+export const navLinks = [
+  {
+    id: "skills",
+    title: "Skills & Experience",
+  },
+  {
+    id: "education",
+    title: "Education",
+  },
+  {
+    id: "achievements",
+    title: "Achievements",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+  },
+  
+  
+  {
+    id: "contactMe",
+    title: "Contact Me",
+  },
+];
+
+export const educationList = [
+  {
+    id: "education-1",
+    icon: oeclogo,
+    title: "Oxford Engineering College, Trichy",
+    degree: "Bachelor of Technology",
+    duration: "July 2020 - July 2024",
+    content1: "Major: Information Technology",
+    
+  },
+  {
+    id: "education-2",
+    icon: pba,
+    title: "Boiler Plant School, Trichy",
+    degree: "12th Standard – Science Stream",
+    duration: "June 2019 - March 2020",
+    content1: "Major: Computer Science",
+  },
+];
+
+export const achievements = [
+  {
+    id: "a-1",
+    icon: googlelogo,
+    event: "Google Data Analytics Professional Certificate",
+    position: "Google",
+    content1: "An industry-recognized certification that teaches data cleaning, analysis, and visualization using tools like Excel, SQL, and Tableau.",
+    content2: " Focused on practical skills and real-world projects.",
+    
+    
+    project: "https://www.coursera.org/account/accomplishments/specialization/TRSXUC9VPUIY",
+    
+  },
+  {
+    id: "a-2",
+    icon: tcslogo,
+    event: "TCS iON Career Edge - Young Professional",
+    position: "TCS iON",
+    content1: "A career-focused course that builds foundational skills like communication, problem-solving, and business etiquette to prepare individuals for professional environments.",
+    content2: "Skills: Presentation Skills · Communication · Soft Skills",
+    content3: "",
+    project: "https://drive.google.com/file/d/1yhy3xSLjRMaFY7NCpDM_n1QaxwicXwVE/view?usp=drivesdk",
+  },
+  {
+    id: "a-3",
+    icon: ibmlogo,
+    event: "Python for Data Science",
+    position: "IBM",
+    content1: "A course focused on Python for data science, covering data manipulation, analysis, and visualization using libraries like Pandas, NumPy, and Matplotlib.",
+    content2: "Skills: Python (Programming Language) · pandas · NumPy",
+    content3: "",
+    project: "https://courses.cognitiveclass.ai/certificates/d1fadb37cddb412c8c4fea0584572739",
+  },
+  {
+    id: "a-4",
+    icon: googlelogo,
+    event: "Share Data Through the Art of Visualization",
+    position: "Google",
+    content1: "A course that teaches effective data visualization techniques to communicate insights clearly.",
+    content2: "Skills: Data Visualization · Data Storytelling · Tableau",
+    content3: "",
+    
+    project: "https://www.coursera.org/account/accomplishments/records/5400ZCIGJBZC",
+  },
+  {
+    id: "a-5",
+    icon: googlelogo,
+    event: "Analyze Data to Answer Questions",
+    position: "Google",
+    content1: "A course focused on data analysis techniques to extract meaningful insights.",
+    content2: "Skills: Data Analysis · Data Aggregation · Data calculations",
+    content3: "",
+    project: "https://www.coursera.org/account/accomplishments/records/P9L2Y4IC8YHN",
+  },
+  
+];
+
+
+export const skills = [
+  {
+    title: "Programming Languages",
+    items: [
+      
+      {
+        id: "pl-2",
+        icon: SiPython,
+        name: "Python",
+      },
+      {
+        id: "pl-3",
+        icon: SiR,
+        name: "R",
+      },
+      {
+        id: "pl-4",
+        icon: SiSqlite,
+        name: "SQL",
+      },
+      
+      {
+        id: "pl-8",
+        icon: AiFillHtml5,
+        name: "HTML",
+      },
+      {
+        id: "pl-9",
+        icon: DiCss3,
+        name: "CSS",
+      },
+      
+    ],
+  },
+  {
+    title: "Data Analysis Skills",
+    items: [
+      {
+        id: "f-1",
+        icon: SiDatabricks,
+        name: "Data Wrangling",
+      },
+      {
+        id: "f-2",
+        icon: SiCkeditor4,
+        name: "Data Manipulation",
+      },
+      {
+        id: "f-3",
+        icon: SiSimpleanalytics,
+        name: "Data Analysis",
+      },
+      {
+        id: "f-4",
+        icon: SiSoundcharts,
+        name: "Data Visualization",
+      },
+      
+    ],
+  },
+  {
+    title: "Tools",
+    items: [
+      {
+        id: "t-1",
+        icon: SiTableau,
+        name: "Tableau",
+      },
+      {
+        id: "t-2",
+        icon: SiMysql,
+        name: "MySQL",
+      },
+      {
+        id: "t-3",
+        icon: SiMicrosoftexcel,
+        name: "Excel",
+      },
+      {
+        id: "t-4",
+        icon: SiVisualstudiocode,
+        name: "VS Code",
+      },
+      {
+        id: "t-5",
+        icon: SiPowerbi,
+        name: "Power Bi",
+      },
+      {
+        id: "t-6",
+        icon: AiFillGithub,
+        name: "GitHub",
+      },
+      {
+        id: "t-7",
+        icon: SiGooglebigquery,
+        name: "Google Bigquery",
+      },
+      {
+        id: "t-8",
+        icon: SiGooglesheets,
+        name: "Google Sheets",
+      },
+      {
+        id: "t-9",
+        icon: SiJupyter,
+        name: "Jupyter Notebook",
+      },
+      
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    organisation: "Codtech IT Solutions, India",
+    logo: codtech,
+    link: "https://www.linkedin.com/company/codtechitsolutions/",
+    positions: [
+      {
+        title: "Data Analyst Intern",
+        duration: "Aug 2024 - Oct 2024",
+        content: [
+          {
+            text: "Conducted a comprehensive analysis of a dataset to uncover trends, patterns, and insights. Utilized Python and libraries such as Pandas and Matplotlib for data manipulation and visualization, resulting in actionable recommendations that enhanced decision-making processes.",
+            link: "",
+          },
+      
+          {
+            text: "Developed complex SQL queries to extract and analyze data from relational databases. Focused on cleaning and transforming data to support business intelligence efforts, significantly improving reporting accuracy and efficiency.",
+            link: "",
+          },
+          
+        ],
+      },
+    ],
+  },
+  
+];
+
+
+
+export const projects = [
+  {
+    id: "project-1",
+    title: "B&J Biscuit Business Analysis",
+    github: "https://github.com/SivaSuriyan-M/B-J-Biscuit-Business-Analysis",
+    
+    image: bj,
+    content: [
+      " Created a comprehensive business analysis dashboards for B&J Biscuit.",
+      " Handled missing data, pre-processed data and cleaned the dataset to ensure accurate analysis.",
+      " Gained insights into revenue distribution, customer demographics, geographic performance, profitability, and sales performance.",
+    ],
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiMicrosoftexcel,
+        name: "Excel"
+      },
+      {
+        id: "icon-2",
+        icon: AiFillDatabase,
+        name: "Data Cleaning"
+      },
+      {
+        id: "icon-3",
+        icon: SiOpenai,
+        name: "Data Pre-Processing"
+      },
+      {
+        id: "icon-4",
+        icon: AiFillCalculator,
+        name: "Pivot"
+      },
+      {
+        id: "icon-5",
+        icon: AiOutlineAreaChart,
+        name: "Data visualization"
+      },
+    ],
+  },
+  {
+    id: "project-2",
+    title: "Exploratory Data Analysis on Marvel Characters",
+    github: "https://github.com/SivaSuriyan-M/Exploratory-Data-Analysis-of-Marvel-Characters",
+    
+    image: marvel,
+    content: [
+      "Performed an in-depth Exploratory Data Analysis (EDA) on a dataset of Marvel comic characters.",
+      " Handled missing data, transformed columns into numeric types, and cleaned the dataset to ensure accurate analysis.",
+      " Gained insights into character popularity, gender representation, alignment, survival status, and debut trends over time.",
+    ],
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiPython,
+        name: "Python"
+      },
+      {
+        id: "icon-2",
+        icon: SiJupyter,
+        name: "Jupyter Notebook"
+      },
+      {
+        id: "icon-3",
+        icon: AiFillCode,
+        name: "EDA"
+      },
+      {
+        id: "icon-4",
+        icon: SiIpfs,
+        name: "Data Cleaning"
+      },
+      {
+        id: "icon-5",
+        icon: AiOutlineBarChart,
+        name: "Data Visualization"
+      },
+      
+    ],
+  },
+  {
+    id: "project-3",
+    title: "Quantifying the Impact of Layoffs: A Comprehensive SQL Analysis",
+    github: "https://github.com/SivaSuriyan-M/Quantifying-the-Impact-of-Layoffs-A-Comprehensive-SQL-Analysis",
+    
+    image: Layoffs1,
+    content: [
+      " Performed SQL-based EDA on a global layoffs dataset to identify trends by company and industry.", 
+      " Created advanced queries to track cumulative layoffs and rank companies, deriving insights on the economic impact, particularly among startups.",
+    ],
+    stack: [
+      {
+        id: "icon-1",
+        icon: AiTwotoneDatabase,
+        name: "SQL"
+      },
+      {
+        id: "icon-3",
+        icon: SiMysql,
+        name: "MySQL"
+      },
+      {
+        id: "icon-4",
+        icon: AiOutlineSearch,
+        name: "Data Exploration"
+      },
+      
+    ],
+  },
+  {
+    id: "project-4",
+    title: "Cleaning Layoffs Data for Advanced Analytics Using SQL",
+    github: "https://github.com/SivaSuriyan-M/Cleaning-Layoffs-Data-for-Advanced-Analytics-Using-SQL",
+    
+    image: Layoffs,
+    content: [
+      " Cleaned and transformed a layoffs dataset using SQL, ensuring accuracy and data integrity by removing duplicates and standardizing names and date formats.",
+      " Handled null values and eliminated unnecessary rows and columns to streamline the dataset.",
+      " Prepared the dataset for exploratory data analysis (EDA) and insights generation.",
+    ],
+    stack: [
+      {
+        id: "icon-1",
+        icon: AiOutlineDatabase,
+        name: "SQL"
+      },
+      {
+        id: "icon-2",
+        icon: SiMysql,
+        name: "MySQL"
+      },
+      {
+        id: "icon-3",
+        icon: AiOutlineTable,
+        name: "Data Cleaning"
+      },
+      
+    ],
+  },
+  {
+    id: "project-5",
+    title: "Advanced Customer Churn Analysis with SQL and Power BI",
+    github: "https://github.com/SivaSuriyan-M/Advanced-Customer-Churn-Analysis-with-SQL-and-Power-BI",
+    
+    image: churn,
+    content: [
+      " Engineered an ETL pipeline using MySQL to streamline customer data processing.", 
+      " Designed and developed a Power BI dashboard to visualize key metrics, analyze churn patterns across demographics, services, distributions and geographic data.",
+      " Enabled actionable insights that drive marketing strategies and customer retention initiatives.",
+    ],
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiMysql,
+        name: "MySQL"
+      },
+      {
+        id: "icon-2",
+        icon: SiPowerbi,
+        name: "Power BI"
+      },
+      {
+        id: "icon-3",
+        icon: SiBuffer,
+        name: "ETL"
+      },
+    ],
+  },
+  {
+    id: "project-6",
+    title: "Electric Vehicle Market Analysis: Insights into BEVs, PHEVs, and Adoption Trends",
+    github: "https://github.com/SivaSuriyan-M/Electric-Vehicle-Market-Analysis-Insights-into-BEVs-PHEVs-and-Adoption-Trends",
+    
+    image: ev,
+    content: [
+      " Conducted a comprehensive analysis of the electric vehicle market using Tableau, focusing on Battery Electric Vehicles (BEVs) and Plug-in Hybrid Electric Vehicles (PHEVs).", 
+      " Explored market trends and identified adoption patterns to understand consumer behavior and preferences.",
+      " Developed key performance indicators to provide actionable insights into growth, regional distribution, and technological advancements.",
+    ],
+    stack: [
+      {
+        id: "icon-1",
+        icon: AiOutlineBarChart,
+        name: "Data Analysis"
+      },
+      {
+        id: "icon-2",
+        icon: AiTwotonePieChart,
+        name: "Data Visualization"
+      },
+      {
+        id: "icon-3",
+        icon: SiTableau,
+        name: "Tableau"
+      },
+    ],
+  },
+  {
+    id: "project-7",
+    title: "Exploratory Data Analysis on NYC Airbnb Listings",
+    github: "https://github.com/SivaSuriyan-M/Exploratory-Data-Analysis-EDA-on-New-York-City-Airbnb-Listings-Dataset",
+    
+    image: airbnb,
+    content: [
+      " Conducted Exploratory Data Analysis on NYC Airbnb listings to uncover market patterns and trends.", 
+      " Cleaned and processed data, addressed missing values, and detected outliers. ",
+      " Visualized geographical distributions and pricing trends using Seaborn",
+      " Analyzed time series data on customer reviews to identify review dynamics over time, utilizing Python for data manipulation and visualization.",
+    ],
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiJupyter,
+        name: "Jupyter Notebook"
+      },
+      {
+        id: "icon-2",
+        icon: SiPython,
+        name: "Python"
+      },
+      {
+        id: "icon-3",
+        icon: SiWpexplorer,
+        name: "EDA"
+      },
+      {
+        id: "icon-4",
+        icon: SiTimescale,
+        name: "Time Series Analysis"
+      },
+    ],
+  },
+  {
+    id: "project-8",
+    title: "Customer Segmentation Using RFM Analysis",
+    github: "https://github.com/SivaSuriyan-M/Customer-Segmentation-Using-RFM-Analysis",
+    
+    image: segmen,
+    content: [
+      " Performed customer segmentation using RFM (Recency, Frequency, Monetary) analysis on retail transactional data.", 
+      " Cleaned and prepared the dataset, engineered features, and classified customers into meaningful segments for targeted marketing strategies.",
+      " Visualized RFM distribution and provided actionable insights to enhance customer retention and boost revenue.",
+    ],
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiJupyter,
+        name: "Jupyter Notebook"
+      },
+      {
+        id: "icon-2",
+        icon: SiPython,
+        name: "Python"
+      },
+      {
+        id: "icon-3",
+        icon: AiOutlineCluster,
+        name: "K-Means Algorithm"
+      },
+    ],
+  },
+  {
+    id: "project-9",
+    title: "Deepfake Detection",
+    github: "https://github.com/SivaSuriyan-M/Deepfake-Detection-Using-Machine-Learning",
+    
+    image: deepfake,
+    content: [
+      " Developed a deepfake detection model using a Convolutional Neural Network (CNN).", 
+      " Explored a semi-supervised learning approach (ResNetV1 + MTCNN) on a subset of the DeepFake Detection Challenge dataset due to its size.",
+      " Demonstrated that this alternative approach can outperform a standard CNN.",
+    ],
+    
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiJupyter,
+        name: "Jupyter Notebook"
+      },
+      {
+        id: "icon-2",
+        icon: SiPython,
+        name: "Python"
+      },
+      {
+        id: "icon-3",
+        icon: AiFillRobot,
+        name: "Machine Learning"
+      },
+    ],
+  },
+  
+];
+
+
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Organisations",
+    value: "2+",
+  },
+  {
+    id: "stats-2",
+    title: "Issues Opened",
+    value: "6+",
+  },
+  {
+    id: "stats-3",
+    title: "Pull Requests",
+    value: "6+",
+  },
+];
+
+
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: AiFillLinkedin,
+    link: "https://www.linkedin.com/in/siva-suriyan",
+  },
+  {
+    id: "social-media-2",
+    icon: AiFillGithub,
+    link: "https://github.com/SivaSuriyan-M",
+  },
+  {
+    id: "social-media-3",
+    icon: AiFillMail,
+    link: "mailto:suriya1571532@gmail.com",
+  },
+  {
+    id: "social-media-4",
+    icon: AiOutlineTwitter,
+    link: "https://www.twitter.com",
+  },
+  {
+    id: "social-media-5",
+    icon: AiFillInstagram,
+    link: "https://www.instagram.com",
+  },
+];
+
+export const aboutMe = {
+    name: "Siva Suriyan",
+    tagLine: "Data Analyst Enthusiast | Crafting Data Stories | SQL, Spreadsheet, Python, Tableau, MySQL | Google Data Analytics Certified | Let's Connect!",
+    intro: " A Passionate Data Analytics enthusiast and a 2024 B.Tech IT graduate. I'm certified in Google Data Analytics and skilled in SQL, Python, and data visualization. I enjoy transforming complex data into actionable insights and impactful stories."
+}
